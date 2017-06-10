9bfb89844f24eeaf692266ed01d9c360221061c6
Jane Wang <jw9billions@gmail.com>
2017-06-09 20:36:48 -0700

    cp7HW

--- models/address_book.rb
+++ models/address_book.rb
@@ -46,6 +46,15 @@ class AddressBook
     end
     return nil
   end
+
+  def iterative_search(fullname)
+    entries.each do |entry|
+      if fullname === entry.name
+        return entry
+      end
+    end
+    return nil
+  end
 end
 
 =begin
